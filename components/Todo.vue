bb197104413ce4269d9914383aee8464c2b9d471
VadyChel <zyzel19@gmail.com>
2022-03-26 20:29:39 +0200

    feat: create first simple todo page and component

---
+++ components/Todo.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="todo" @mouseenter="hoverableButtons = true" @mouseleave="hoverableButtons = false">
+  <div :class="{'hoverable-buttons': true, 'd-none': !hoverableButtons}">
+    <v-icon class="hoverable-button new-todo-button hoverable-icon" color="var(--todos-list)">mdi-plus</v-icon>
+    <v-icon class="hoverable-button interaction-button hoverable-icon" color="var(--todos-list)">mdi-dots-grid</v-icon>
+    <div class="hoverable-button"></div>
+  </div>
+  <span class="todo-content">{{ content }}</span>
+</div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    content: {
+      type: String,
+      required: true
+    },
+    position: {
+      type: Number,
+      required: true
+    },
+    styles: {
+      type: Object,
+      required: true
+    },
+    author: {
+      type: String,
+      required: true
+    }
+  },
+  data: () => ({
+    hoverableButtons: false
+  })
+}
+</script>
+
+<style scoped>
+.todo {
+  padding: 5px;
+  position: relative;
+}
+
+.todo-content {
+  font-size: 1.05em;
+  font-weight: 450;
+}
+
+.hoverable-button {
+  position: absolute;
+  bottom: 0;
+  top: 0;
+}
+
+.new-todo-button {
+  left: -60px;
+}
+
+.interaction-button {
+  left: -30px;
+}
+</style>
--- pages/user/_pageID.vue
+++ pages/user/_pageID.vue
@@ -1,16 +1,65 @@
 <template>
-<div>
-
+<div class="page-content">
+  <div class="page-todos">
+    <span class="page-title">Todo Page Name</span>
+    <div v-for="todo of pageTodos" :key="todo.id">
+      <todo :content="todo.content" :position="todo.position" :styles="todo.styles" :author="todo.author"/>
+    </div>
+  </div>
+  <div class="adding-todo-area" @click="addTodo"></div>
 </div>
 </template>
 
 <script>
 export default {
   name: "TodoPage",
-  layout: 'todo'
+  layout: 'todo',
+  data: () => ({
+    newTodos: []
+  }),
+  computed: {
+    pageTodos () {
+      return [...this.savedTodos, ...this.newTodos].sort((todo1, todo2) => {
+        return todo1.position < todo2.position ? -1
+          : todo2.position < todo1.position ? 1 : 0
+      })
+    },
+    savedTodos: () => [
+      { id: 0, position: 0, content: '1 Todo' },
+      { id: 1, position: 1, content: '2 Todo' },
+      { id: 2, position: 2, content: '3 Todo' },
+    ] // Fetched from api
+  },
+  methods: {
+    addTodo () {
+      this.newTodos.push({
+        id: this.pageTodos[this.pageTodos.length-1].id+1,
+        position: this.pageTodos.length+1,
+        content: 'gg',
+        styles: {},
+        author: ''
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
+.page-content {
+  padding: 8em;
+  height: 100%;
+}
+
+.page-title {
+  font-size: 4em;
+  position: relative;
+  font-weight: 550;
+  z-index: 10;
+}
+
+.adding-todo-area {
+  height: 100%;
+  cursor: text;
+}
 
 </style>
