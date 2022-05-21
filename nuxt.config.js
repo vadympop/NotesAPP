58f51e7bb82a20abeafa1efe7029ff97ef4571fe
VadyChel <zyzel19@gmail.com>
2022-05-21 16:53:10 +0300

    feat: change pages title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,7 @@ import { API_URL } from './consts'
 
 export default {
   head: {
-    titleTemplate: '%s - todo',
-    title: 'todo',
+    title: 'NotesAPP',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
