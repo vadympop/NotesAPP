1dd292589a2b0136ce81576d139706caa637589a
VadyChel <zyzel19@gmail.com>
2022-07-13 13:54:40 +0300

    feat: added button "Send activation mail AGAIN"

--- components/UserNotActivatedBlank.vue
+++ components/UserNotActivatedBlank.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="blank">
-    Your account isn't activated! View activation mail in your email
+    <span>Your account isn't activated! View activation mail in your email</span>
+    <v-btn depressed small color="teal darken-2" class="ml-10" @click="sendActivationMail">
+      Send activation mail AGAIN
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'UserNotActivatedBlank'
+  name: 'UserNotActivatedBlank',
+  methods: {
+    async sendActivationMail () {
+      await this.$axios.post('users/activate/send')
+      this.$store.commit('updateSnackbar', {
+        state: true,
+        message: "Activation mail sent successfully",
+        type: 'success',
+        apiError: false
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
 .blank {
   background-color: var(--yellow);
-  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   color: var(--dark-gray);
   font-weight: 500;
   font-size: 0.9em;
